790b1fdffc5f10e992bf9a9cca1524f4e7f5e042
SteveAdeiza <105511911+SteveAdeiza@users.noreply.github.com>
2026-05-27 13:06:56 +0100

    Add stories.js with initial story data

---
+++ data/stories.js
@@ -0,0 +1,9 @@
+export const stories = {
+  "into-the-badlands": {
+    title: "Into The Badlands",
+    author: "Mr. Steve The Phoenix",
+    season1: {},
+    season2: {},
+    season3: {}
+  }
+}
